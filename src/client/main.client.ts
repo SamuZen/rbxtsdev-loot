e86f842a4dfce02e9fe82f2a5a42f7432f400f52
PedroQueirozHut <pedro.queiroz@manifestohut.com.br>
2023-04-07 01:01:14 -0300

    Add listener to client events - debug loot position on event call

--- src/client/main.client.ts
+++ src/client/main.client.ts
@@ -1,3 +1,24 @@
-import { InitializeLootClient } from "@rbxts/loot/out/client/loot";
+import * as LootClient from "@rbxts/loot/out/client/loot";
 
-InitializeLootClient(40);
+LootClient.InitializeLootClient();
+LootClient.setPlayerCollectorSize(5);
+
+const workspace = game.GetService("Workspace");
+
+function create(pos: Vector3) {
+	const part = new Instance("Part");
+	part.Size = new Vector3(1, 1, 1);
+	part.Anchored = true;
+	part.CanCollide = false;
+	part.PivotTo(new CFrame(pos));
+	part.Parent = workspace;
+}
+
+LootClient.onLootCreated((lootData) => {
+	warn("Loot created: ", lootData);
+});
+
+LootClient.onLootCollected((lootData) => {
+	create(lootData.loot.handle.GetPivot().Position);
+	warn("Loot collected: ", lootData);
+});
